fce05709fa429f931f78d62a8b07f297023d7f62
PeterOsoo <posoo617@gmail.com>
2020-12-22 11:46:12 +0300

    birthday reminder project

---
+++ 01 birthday reminder/src/App.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react"
+import data from "./data"
+import List from "./List"
+
+import "./App.css"
+
+const App = () => {
+	const [people, setPeople] = useState(data)
+
+	return (
+		<main>
+			<section className="container">
+				<h3>{people.length} birthdays today</h3>
+
+				<List people={people} />
+
+				<button onClick={() => setPeople([])}>
+					{" "}
+					{people.length > 0 ? "clear all" : "No birthdays"}{" "}
+				</button>
+			</section>
+		</main>
+	)
+}
+
+export default App
