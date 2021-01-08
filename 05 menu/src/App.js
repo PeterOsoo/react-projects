0e28aa8a802ea1e665ea49cd50664c190e224b83
PeterOsoo <posoo617@gmail.com>
2021-01-08 09:24:25 +0300

    menu

--- 05 menu/src/App.js
+++ 05 menu/src/App.js
@@ -1,8 +1,37 @@
-import logo from "./logo.svg"
+import React, { useState } from "react"
+import Menu from "./Menu"
+import Categories from "./Categories"
+import items from "./data"
+
 import "./App.css"
 
-function App() {
-	return <div>Menu App</div>
+const allCategories = ["all", ...new Set(items.map(item => item.category))]
+
+const App = () => {
+	const [menuItems, setMenuItems] = useState(items)
+	const [categories, setCategories] = useState(allCategories)
+
+	const filterItems = category => {
+		if (category === "all") {
+			setMenuItems(items)
+			return
+		}
+		const newItems = items.filter(item => item.category === category)
+		setMenuItems(newItems)
+	}
+
+	return (
+		<main>
+			<section className="menu section">
+				<div className="title">
+					<h2>our menu</h2>
+					<div className="underline"></div>
+				</div>
+				<Categories categories={categories} filterItems={filterItems} />
+				<Menu items={menuItems} />
+			</section>
+		</main>
+	)
 }
 
 export default App
